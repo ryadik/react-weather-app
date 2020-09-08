489335cf081a1ac824025ebf8a388146377b3d01
K1nGsmaN-hub <15162342h@gmail.com>
2020-09-08 13:36:07 +0800

    added service function

---
+++ src/service/getWeatherData.js
@@ -0,0 +1,11 @@
+const getWeatherData = async () => {
+  const res = await fetch('https://api.openweathermap.org/data/2.5/forecast?id=2023469&appid=a6177223297e1975f13fa9e9805475ae')
+
+  if (!res.ok) {
+    throw new Error(`${res.status}`)
+  }
+
+  return res.json()
+}
+
+export {getWeatherData}
