84b181eaddf9cd034219a0316425fed09dcb652d
K1nGsmaN-hub <15162342h@gmail.com>
2020-09-08 22:17:44 +0800

    added hooks (state) to main-page

--- src/components/main-page/main-page.js
+++ src/components/main-page/main-page.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import {NavLink} from "react-router-dom";
 
 import './main-page.sass'
 
 const MainPage = ({changeCityName}) => {
+  const [inputData, changeInputData] = useState('')
+
   return (
       <div className="main-page">
         <h1 className="main-page__title">Weather App</h1>
 
         <div className="main-page__text-field">
-          <input onChange={changeCityName} type="text" placeholder="Enter a city"/>
-            <NavLink to="/weather"><span>&#10148;</span></NavLink>
+          <input onChange={(e) => changeInputData(e.target.value)} type="text" placeholder="Enter a city"/>
+          <NavLink onClick={() => changeCityName(inputData)} to="/weather"><span>&#10148;</span></NavLink>
         </div>
       </div>
   )
